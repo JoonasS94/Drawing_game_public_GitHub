0d7c86f620ba21c985d3bcec9057eedbbf0a46c3
Joonas Sariola <91365380+JoonasS94@users.noreply.github.com>
2022-12-20 10:47:24 +0200

    Update GameManagerTutorial.cs

--- Assets/Scripts/Tutorial/GameManagerTutorial.cs
+++ Assets/Scripts/Tutorial/GameManagerTutorial.cs
@@ -15,8 +15,7 @@ public class GameManagerTutorial : MonoBehaviour
         float dist = Vector3.Distance(go1.transform.position, go2.transform.position);
 
         if (dist > 0.15f)
-        { 
-            //Debug.Log("Distance grown, I should move now");
+        {
             go2.transform.position = new Vector3(go1.transform.position.x, -0.0035f, go1.transform.position.z);
         }
     }
