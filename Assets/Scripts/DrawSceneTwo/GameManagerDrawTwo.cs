59160441db1a725db550e71a337c23a45a517a3c
Joonas Sariola <91365380+JoonasS94@users.noreply.github.com>
2022-12-20 10:48:22 +0200

    Update GameManagerDrawTwo.cs

--- Assets/Scripts/DrawSceneTwo/GameManagerDrawTwo.cs
+++ Assets/Scripts/DrawSceneTwo/GameManagerDrawTwo.cs
@@ -61,7 +61,6 @@ public class GameManagerDrawTwo : MonoBehaviour
 
         if (dist > 0.15f && stillDrawing == true)
         { 
-            //Debug.Log("Distance grown, I should move now");
             go2.transform.position = new Vector3(go1.transform.position.x, -0.0035f, go1.transform.position.z);
             lasku = (lasku - 1);
             UpdateInkLeft(lasku);
